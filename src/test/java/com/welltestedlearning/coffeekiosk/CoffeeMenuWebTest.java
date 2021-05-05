f59729261ed29761c988ecfc471f6a9b13cd4232
Ted M. Young <tedyoung@gmail.com>
2021-05-05 11:24:19 -0700

    Removed unnecessary classes. Created failing test.

---
+++ src/test/java/com/welltestedlearning/coffeekiosk/CoffeeMenuWebTest.java
@@ -0,0 +1,23 @@
+package com.welltestedlearning.coffeekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CoffeeMenuController.class)
+public class CoffeeMenuWebTest {
+
+  // this is Field Injection - this OK in tests
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void getFromCoffeeSizesEndpointReturns200Ok() throws Exception {
+    mockMvc.perform(get("/api/coffee/menu/sizes"))
+           .andExpect(status().isOk());
+  }
+}
