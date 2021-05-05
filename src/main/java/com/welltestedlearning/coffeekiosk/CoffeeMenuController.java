4f33d98f513fd22e1f5c67edb5ece74dd9d344c5
Ted M. Young <tedyoung@gmail.com>
2021-05-05 11:36:28 -0700

    Made test pass by adding GET-mapped endpoint.

--- src/main/java/com/welltestedlearning/coffeekiosk/CoffeeMenuController.java
+++ src/main/java/com/welltestedlearning/coffeekiosk/CoffeeMenuController.java
@@ -1,7 +1,15 @@
 package com.welltestedlearning.coffeekiosk;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoffeeMenuController {
+
+  @GetMapping("/api/coffee/menu/sizes")
+//  @RequestMapping(value = "/api/coffee/menu/sizes", method = RequestMethod.GET)
+  public String getCoffeeSizes() {
+    return "small";
+  }
+
 }
