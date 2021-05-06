06cedec0d59223318a9a14dce8cf39213f109fee
Ted M. Young <tedyoung@gmail.com>
2021-05-06 07:36:14 -0700

    Post Lab 8: Remover Support for XML

--- src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
+++ src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
@@ -1,15 +1,13 @@
 package com.welltestedlearning.coffeekiosk.adapter.in.api;
 
 import com.welltestedlearning.coffeekiosk.domain.CoffeeItem;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoffeeOrderController {
 
-  @GetMapping(value = "/api/coffee/order",
-      produces = {MediaType.APPLICATION_XML_VALUE} )
+  @GetMapping("/api/coffee/order")
   public CoffeeItemResponse coffeeItem() {
     CoffeeItem coffeeItem = new CoffeeItem("small", "latte", "milk");
     coffeeItem.setId(1L);
