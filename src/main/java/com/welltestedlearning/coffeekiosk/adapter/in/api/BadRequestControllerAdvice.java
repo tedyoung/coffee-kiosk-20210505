656d9d2c81679289c3e6f1b26762cec46ac2192a
Ted M. Young <tedyoung@gmail.com>
2021-05-11 10:00:28 -0700

    Lab 10g: Add custom error response.

--- src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/BadRequestControllerAdvice.java
+++ src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/BadRequestControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.welltestedlearning.coffeekiosk.adapter.in.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +11,14 @@ public class BadRequestControllerAdvice {
 
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public void handleIllegalArgumentAsBadRequest() {
+  public ResponseEntity<ErrorResponse> handleIllegalArgumentAsBadRequest(
+      IllegalArgumentException exception) {
+
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            exception.getMessage()));
   }
 
 }
